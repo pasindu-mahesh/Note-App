ce874838b10b0d7b8353f0e236f525eec35d8e59
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-09 10:43:07 +0530

    feat: surver runing

--- server/index.js
+++ server/index.js
@@ -4,3 +4,11 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.json("Hello from the server! This is the backend of the Note App.");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
