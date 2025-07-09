e27bddc47d24bae60dcace4a1c1a21a0619b44bc
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-09 10:59:16 +0530

    feat:updating index

--- server/index.js
+++ server/index.js
@@ -5,8 +5,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.get("/", (req, res) => {
     res.json("Hello from the server! This is the backend of the Note App.");
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not Found");
 });
 
 app.listen(PORT, () => {
