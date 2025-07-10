f835d3378747c72cc7d2ea11f08bf1c27084ea62
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-10 08:18:40 +0530

    feat: get all notes

--- server/index.js
+++ server/index.js
@@ -13,9 +13,25 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//get all notes
+app.get("/api/note", async (req, res) => {
+
+    try{
+
+        const data = await note.find({})
+
+        if(!data){
+            throw new Error('An error occurred while fetching notes.')
+        }
+        res.status(201).json(data);
+    }catch ( error) {
+        res.status(500).json({ error: 'An error occurred while fetching notes...'})
+    }
+});
+
 
 app.get("/", (req, res) => {
-    res.json("Hello from the server! This is the backend of the Note App.");
+    res.json("Hello from the server! This is the backend of the note app!");
 });
 
 app.use((req, res) => {
