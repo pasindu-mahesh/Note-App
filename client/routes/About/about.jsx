214ad4d031023b10ff011fa193067ff33ba36ed5
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-11 18:07:21 +0530

    feat. add about and home page

---
+++ client/routes/About/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+        <p>About</p>
+        <p>This is a simple note-taking application built with React.</p>
+        <p>It allows users to create, edit, and delete notes.</p>
+    </div>
+  );
+}
+
+export default About;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from '/routes/Home/home.jsx';
+import About from '/routes/About/about.jsx';
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        Header
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        Footer
+      </Router>
     </>
   )
 }
