afdc234842dc52a12718e2e8f4b50b45a9f2aea3
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-12 13:31:00 +0530

    feat: fetching data

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Notes() {
-  return (
-    <div>
-      Notes
-    </div>
-  )
+    const baseUrl = `${import.meta.env.VITE_SERVER_URL}/api/note`;
+    const [data, setData] = useState([]);
+    const [loading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(baseUrl);
+
+                if (!response.ok) {
+                    throw new Error("Faild to fetch data")
+                }
+
+                const data = await response.json();
+                setData(data);
+                setIsLoading(false);
+
+
+            } catch (error) {
+                setError("Error fetching data.please try again later");
+                setIsLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+
+
+    return (
+        <div>
+
+            <pre>
+                {JSON.stringify(data, null, 2)}
+            </pre>
+
+
+        </div>
+    );
 }
 
-export default Notes
+export default Notes;
