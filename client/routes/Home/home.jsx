eadf158eac4e501d1f30a32375ab59850013c2a5
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-12 11:41:00 +0530

    feat:create, note page

--- client/routes/Home/home.jsx
+++ client/routes/Home/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Notes from '../../src/components/Notes.jsx';
+
 
 function Home() {
   return (
     <main>
       <h1>Home</h1>
       <p>Welcome to the note-taking app.</p>
+
+      <Notes />
     </main>
   )
 }
