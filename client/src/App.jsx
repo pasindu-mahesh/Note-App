3f15c3934ebda4de89640814fe5b7c30a0740ea2
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-12 09:28:11 +0530

    feat: complete basic header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from '/routes/Home/home.jsx';
 import About from '/routes/About/about.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
 
   return (
     <>
       <Router>
-        Header
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
