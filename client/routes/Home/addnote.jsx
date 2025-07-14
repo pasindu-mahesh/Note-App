03b432d2300db232be99a03d714eb0da88fa3944
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-14 10:48:24 +0530

    feat: note createing success full

--- client/routes/Home/addnote.jsx
+++ client/routes/Home/addnote.jsx
@@ -3,21 +3,37 @@ import { Link } from 'react-router-dom';
 
 function AddNote() {
 
-  
-  
-
+  const baseUrl = `${import.meta.env.VITE_SERVER_URL}/api/note`;
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
   const addNote = async (e) => {
 
+    e.preventDefault();
+
+    try {
+      const response = await fetch(baseUrl,{
+        method: "POST",
+        headers: { "Content-Type" : "application/json" },
+        body: JSON.stringify({ 
+          title, description
+        }),
+      })
+
+    } catch (error) {
+      console.error(error);
+    }
+
   }
+
   return (
     <div>
       <Link to="/" className='back-button'> ⏴Back </Link>
 
       <form onSubmit={addNote}>
+
         <div className='single-note'>
+
           <div>
             <input
               type="text"
@@ -27,6 +43,7 @@ function AddNote() {
               className='title'
             />
           </div>
+
           <div>
             <textarea
               value={description}
@@ -36,12 +53,15 @@ function AddNote() {
               cols="50"
               className='description'
               >
-                
               </textarea>
           </div>
+
         </div>
+
         <input type="submit"/>
+
       </form>
+
     </div>
   )
 }
