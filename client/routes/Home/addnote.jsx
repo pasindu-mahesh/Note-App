39a267722b6ad7ff64f2ca64bb1615ea933b8e39
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-14 10:34:22 +0530

    feat: add title area, description area and submit btm

--- client/routes/Home/addnote.jsx
+++ client/routes/Home/addnote.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 function AddNote() {
+
+  
+  
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const addNote = async (e) => {
+
+  }
   return (
     <div>
-      AddNote
+      <Link to="/" className='back-button'> ⏴Back </Link>
+
+      <form onSubmit={addNote}>
+        <div className='single-note'>
+          <div>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder='Title'
+              className='title'
+            />
+          </div>
+          <div>
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder='Description'
+              rows="4"
+              cols="50"
+              className='description'
+              >
+                
+              </textarea>
+          </div>
+        </div>
+        <input type="submit"/>
+      </form>
     </div>
   )
 }
